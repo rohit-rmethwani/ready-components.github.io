38c367b819edef563710c6b25bea9d201db676d4
Rohit Methwani <rohit.methwan31@gmail.com>
2020-09-11 21:20:59 +0530

    Added hotjar script

--- js/script.js
+++ js/script.js
@@ -63,4 +63,13 @@ $(function () {
 			scrollTop: $(section).offset().top - 69,
 		},1250, "easeInOutExpo");
 	});
+	
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1987591,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 });
